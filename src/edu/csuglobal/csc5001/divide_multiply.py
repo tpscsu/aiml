12fe19b9873de25e8d0da994327e48ac55e85893
tiginsamuel <tiginsamuel@gmail.com>
2024-09-11 21:27:25 -0500

    Initial commit for CSC500-1 week1 task

--- src/edu/csuglobal/csc5001/divide_multiply.py
+++ src/edu/csuglobal/csc5001/divide_multiply.py
@@ -1,16 +1,17 @@
 # The below program accepts two numbers from terminal num1 and num2
-# Adds the two numbers and output the result on the terminal
-# Subtract the numbers in both ways and output the results on the terminal
+# Multiplies the two numbers and output the result on the terminal
+# Divides num1 by num2 and output the result on the terminal
 # The program will terminate with an error message if a non-numeric input is provided
 
 
 try:
     num1 = float(input('Enter the first number : '))
     num2 = float(input('Enter the second number : '))
-    total = num1 + num2
-    difference = num1 - num2
-    print(num1, '+', num2, '=', total)
-    print(num1, '-', num2, '=', difference)
+    product = num1 * num2
+    quotient = num1 / num2
+    print(num1, '*', num2, '=', product)
+    print(num1, '/', num2, '=', quotient)
 except ValueError:
     print('Invalid Inputs - Expected a number')
     exit()
+
